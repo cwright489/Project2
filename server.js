fd290d1d87157bbb9dcf9dd149df2659e97028ad
Chance Wright <cwright489@gmail.com>
2020-03-22 13:48:20 -0600

    Added config, models, routes, updated the server.js file, added the public routes to the signup and login page. added and got working Passport to facilitate the signup and login. created a database to hold users and collect emails and passwords. made the passwords come through to the sql table encrypted.

---
+++ server.js
@@ -0,0 +1,37 @@
+// Requiring necessary npm middleware packages 
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("./config/passport");
+// Setting up port
+var PORT = process.env.PORT || 8080;
+//Import the models folder
+var db = require("./models");
+// Creating express app and configuring middleware 
+//needed to read through our public folder
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
+app.use(express.static("public"));
+//
+//we are doing a GET to test if our server is working fine
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+//
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+//
+//this will listen to and show all activities on our terminal to 
+//let us know what is happening in our app
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
+  });
